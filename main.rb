003d502c494a9637a9c90e700ac22952934dbbeb
pmiskiewicz <piotr.miskiewicz.priv@gmail.com>
2021-04-11 22:50:18 +0200

    read, parse and display questions

--- main.rb
+++ main.rb
@@ -1,1 +1,8 @@
-# main class in my app
+require 'csv'
+
+file_location = 'questions.csv'
+content = File.read(file_location)
+
+data = CSV.parse(content, headers: true)
+
+data.each {|q| puts "Pytanie za #{q['step']}:\n#{q['question']}\nA) #{q['answer_1']}\nB) #{q['answer_2']}\nC) #{q['answer_3']}\nD) #{q['answer_4']}"}
