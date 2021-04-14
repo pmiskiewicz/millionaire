448dcbfaa7ca9d86b9b41f79e021f5d8fda444c3
Piotr Miśkiewicz <38345560+pmiskiewicz@users.noreply.github.com>
2021-04-14 22:16:22 +0200

    Delete main.rb

--- main.rb
+++
@@ -1,8 +0,0 @@
-require 'csv'
-
-file_location = 'questions.csv'
-content = File.read(file_location)
-
-data = CSV.parse(content, headers: true)
-
-data.each {|q| puts "Pytanie za #{q['step']}:\n#{q['question']}\nA) #{q['answer_1']}\nB) #{q['answer_2']}\nC) #{q['answer_3']}\nD) #{q['answer_4']}"}
